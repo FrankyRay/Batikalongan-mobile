43044ac9538cfb71f5ad31c82b3f334638222b5c
FrankyRay <frankyraymarcellsinaga@gmail.com>
2024-12-23 00:10:36 +0700

    Fix all unmerged code

--- lib/article/screens/artikel_form_screen.dart
+++ lib/article/screens/artikel_form_screen.dart
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-import 'package:pbp_django_auth/pbp_django_auth.dart';
-import 'package:provider/provider.dart';
-import '../widgets/entry_form.dart';
-
-class ArtikelFormScreen extends StatelessWidget {
-  const ArtikelFormScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    context.watch<CookieRequest>();
-    return Scaffold(
-      appBar: AppBar(title: const Text('Tambah Artikel')),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ArtikelFormWidget(
-          onSubmit: (artikel) {
-            Navigator.pop(context, artikel);
-          },
-        ),
-      ),
-    );
-  }
-}
-=======
 import 'package:flutter/material.dart';
 import 'package:pbp_django_auth/pbp_django_auth.dart';
 import 'package:provider/provider.dart';
@@ -89,4 +63,3 @@ class ArtikelFormScreen extends StatelessWidget {
     );
   }
 }
->>>>>>> f7040ecc866004223017a11a8dc0615001dc53ca
